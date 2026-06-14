89d1206e0dfce505a74380ce89052db654666e59
Vaishnavi Bongurala <vaishnavibongurala@gmail.com>
2026-06-15 00:22:40 +0530

    Latest Changes

--- LibraryManagement/settings.py
+++ LibraryManagement/settings.py
@@ -133,8 +133,16 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
+############ LOCAL ###################################
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+#-----------------------------------------------------
+
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
+
 
 EMAIL_HOST_USER = 'vaishnavibmethodist@gmail.com'
 EMAIL_HOST_PASSWORD = 'wisc wtuc bzsx kvel' #wisc wtuc bzsx kvel
