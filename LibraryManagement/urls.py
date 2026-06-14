88f540419d753eecabc62a730242963b56b664e6
Vaishnavi Bongurala <vaishnavibongurala@gmail.com>
2026-06-14 23:36:00 +0530

    Latest Changes

--- LibraryManagement/urls.py
+++ LibraryManagement/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/login/', TokenObtainPairView.as_view()),
+    path('api/refresh/', TokenRefreshView.as_view()),
+
     path('', include('library.urls')),
+
+
 ]
